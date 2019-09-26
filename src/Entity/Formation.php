346581bc4cfa97cbb6298645d68159f2e9b888fa
DarkChambers <monteilchristophe@live.fr>
2019-09-26 10:22:21 +0200

    add Formation entity, ToDo : create for fixtures and fake data, do the same for education and more, add access control and user, fixing images by asset

---
+++ src/Entity/Formation.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FormationRepository")
+ */
+class Formation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $school;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $degree;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $period;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Cv", inversedBy="formations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $cv;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSchool(): ?string
+    {
+        return $this->school;
+    }
+
+    public function setSchool(string $school): self
+    {
+        $this->school = $school;
+
+        return $this;
+    }
+
+    public function getDegree(): ?string
+    {
+        return $this->degree;
+    }
+
+    public function setDegree(string $degree): self
+    {
+        $this->degree = $degree;
+
+        return $this;
+    }
+
+    public function getPeriod(): ?string
+    {
+        return $this->period;
+    }
+
+    public function setPeriod(string $period): self
+    {
+        $this->period = $period;
+
+        return $this;
+    }
+
+    public function getCv(): ?Cv
+    {
+        return $this->cv;
+    }
+
+    public function setCv(?Cv $cv): self
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->getTitle();
+    }
+}
